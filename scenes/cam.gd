ab0f1eceb7cb0e787be515a77e229f6f78f40c70
plasalt <22253@gdc.school.nz>
2026-08-04 09:45:20 +1200

    cam setup and marker rewrite

---
+++ scenes/cam.gd
@@ -0,0 +1,10 @@
+extends Camera2D
+
+var speed = 10.0 
+var direction : Vector2
+
+func _input(_event):
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+func _process(delta: float) -> void:
+	position += direction * speed * -1 
--- scripts/control.gd
+++ scripts/control.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var marker = $marker
 @onready var bounds = $bounds
 @export var clock: Node
+@onready var time = $Camera2D/time
+@onready var cords = $Camera2D/cords
+const Marker = preload("res://scenes/marker.tscn")
 
 func _ready():
 	player.global_position = GameData.playerpos
@@ -16,8 +19,13 @@ func _process(delta):
 	bounds.boundingcheck()
 	bounds.resettriggers()
 	
-	$cords.text = str(player.global_position.floor())
-	$time.text = str(clock.formate_time())
+	cords.text = str(player.global_position.floor())
+	time.text = str(clock.formate_time())
+	
+	if is_instance_valid(marker):
+		player.target_position = marker.global_position
+		player.move_to(marker.global_position)
+		
 	pass
 
 func _input(event):
@@ -26,7 +34,14 @@ func _input(event):
 	and event.button_index == MOUSE_BUTTON_LEFT:
 
 		var click_pos = get_global_mouse_position()
-
-		marker.global_position = click_pos
-		player.target_position = click_pos
-		player.move_to(click_pos)
+		
+		var click_radius = 19.0
+		if is_instance_valid(marker):
+			if click_pos.distance_to(marker.global_position) <= click_radius:
+				marker.queue_free()
+				return
+		else: 
+			marker = Marker.instantiate()
+			marker.global
+			
+			
